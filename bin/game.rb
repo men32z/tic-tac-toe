d6b92c4e29c70a3292fbced97848a9e0100b3e70
Luis Preza <preza.luis@gmail.com>
2019-10-09 17:20:23 -0500

    status game

--- bin/game.rb
+++ bin/game.rb
@@ -1,6 +1,6 @@
 class Game
   attr_accessor :board
-  
+
   def initialize
     @turn_x = true
     @board = [nil]*9
@@ -14,9 +14,19 @@ class Game
 
     @board[position-1] = (@turn_x) ? 'X': 'O'
     @turn_x = !@turn_x
-    #status_game()
+    status_game(position-1)
   end
 
+  def status_game(last_p)
+    current_value = @turn_x ? "O" : "X"
+    winner_cases = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    winner_cases.each do |x|
+      if x.include? last_p
+        return current_value if current_value == @board[x[0]] && current_value == @board[x[1]] && current_value == @board[x[2]]
+      end
+    end
+    false
+  end
 
   def show_board
     n_s = [1, 4, 7]
