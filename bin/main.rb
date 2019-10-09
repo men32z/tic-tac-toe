fe3b590f388880aeedfa70d070b9a7d9ee425c06
Luis Preza <preza.luis@gmail.com>
2019-10-09 11:51:59 -0500

    welcome message

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts "Hello World!!"
+puts "Hello welcome to the Tic Tac Toe Game"
+puts "--------------------------------------"
+puts "This our board"
+puts "_1_|_2_|_3_"
+puts "_4_|_5_|_6_"
+puts " 7 | 8 | 9 "
+puts "pick a number to set the first turn 'X' "
