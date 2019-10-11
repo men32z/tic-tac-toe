1177648f74a8065c4650c5d21e313be8c92ab84a
Luis Preza <preza.luis@gmail.com>
2019-10-11 13:07:37 -0500

    game logic finished

--- bin/game.rb
+++ bin/game.rb
@@ -1,8 +1,22 @@
 class Game
+  attr_accessor :user
   attr_reader :total_turns
 
   def initialize
     reset_game
+  end
+
+  private
+
+  def winner(last_p)
+    current_value = @turn_x ? "O" : "X"
+    winner_cases = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    winner_cases.each do |x|
+      if x.include? last_p
+        return current_value if current_value == @board[x[0]] && current_value == @board[x[1]] && current_value == @board[x[2]]
+      end
+    end
+    false
   end
 
   public
@@ -17,23 +31,16 @@ class Game
     winner = winner(position-1)
     if !winner && total_turns < 1
       return "It's a draw"
-    elsif !winner 
+    elsif !winner
       return winner
     else
       @total_turns = 0
-      return "The champion is #{winner}!!"
+      return "The champion is #{@user[winner.downcase.to_sym]} '#{winner}'!!"
     end
   end
 
-  def winner(last_p)
-    current_value = @turn_x ? "O" : "X"
-    winner_cases = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-    winner_cases.each do |x|
-      if x.include? last_p
-        return current_value if current_value == @board[x[0]] && current_value == @board[x[1]] && current_value == @board[x[2]]
-      end
-    end
-    false
+  def current_turn
+    @turn_x ? 'X': 'O'
   end
 
   def show_board
@@ -41,7 +48,11 @@ class Game
     board = ""
     9.times { |x|
       board +=  (n_s.include? x) ? "|" : ""
-      board += " #{x+1} "
+      if @total_turns == 9
+        board +=  " #{x+1} "
+      else
+        board +=  " #{@board[x] ? @board[x] : ' '} "
+      end
       board +=  (n_s.include? x) ? "|" : ""
       board +=  (n_s.include? x+2) ? "\n---|---|---\n" : ""
     }
@@ -52,5 +63,11 @@ class Game
     @turn_x = true
     @board = [nil]*9
     @total_turns = 9
+    if @user.nil?
+      @user = {
+        x: '',
+        o: ''
+      }
+    end
   end
 end
--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,45 @@
 #!/usr/bin/env ruby
 require_relative 'game'
 puts "Hello welcome to the Tic Tac Toe Game"
+
+game = Game.new
+
 puts "What is your name? (player X)"
-player_x = gets.chomp
+game.user[:x] = gets.chomp
 puts "What is your name? (player O)"
-player_o = gets.chomp
+game.user[:o] = gets.chomp
 puts "--------------------------------------"
 puts "This our board"
-game = Game.new
+
 puts game.show_board
-
- while game.total_turns > 0
-
-  puts "Pick a number to set the turn 'X' "
-  position = gets.chomp
-  #if move is valid
-  status_game = game.move_game(position.to_i)
-  if status_game
-    puts status_game
+flag = true
+while flag
+  flag = false
+  while game.total_turns > 0
+    puts "#{game.user[game.current_turn.downcase.to_sym]} pick a number to set the turn '#{game.current_turn}' "
+    position = gets.chomp
+    #if move is valid
+    puts "--------------------------------------"
+    status_game = game.move_game(position.to_i)
+    if status_game
+      puts status_game
+    else
+      puts "Next turn"
+    end
+    #Gem.win_platform? ? (system "cls") : (system "clear")
+    puts "--------------------------------------"
+    puts game.show_board
   end
-  puts "Now, your move is displayed on the board"
-  puts "--------------------------------------"
-  puts game.show_board
+
+  puts ""
+  puts "Do you want to play again? (y/n):"
+  answer = gets.chomp
+  if answer.downcase == 'y'
+    flag = true
+    game.reset_game
+    puts "--------------------------------------"
+    puts game.show_board
+  else
+    puts "Bye Bye!!"
+  end
 end
