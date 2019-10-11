a1fd5385bc61732e39b49c10f863876f188986e5
Luis Preza <preza.luis@gmail.com>
2019-10-11 10:38:11 -0500

    adding sugesting changes

--- bin/main.rb
+++ bin/main.rb
@@ -5,10 +5,19 @@ puts "This our board"
 puts "_1_|_2_|_3_"
 puts "_4_|_5_|_6_"
 puts " 7 | 8 | 9 "
-puts "Pick a number to set the turn 'X' "
-position_x = gets.chomp
-puts "Now, your move is displayed on the board"
-puts "Pick a number to set the turn 'O' "
-position_o = gets.chomp
-puts "Now, your move is displayed on the board"
-puts "to be continued.."
+
+there_turns = 9
+
+while there_turns > 0
+
+  puts "Pick a number to set the turn 'X' "
+  move = gets.chomp
+  #if move is valid
+  puts move ? "your move is valid" : "your move is not valid"
+
+  puts "Now, your move is displayed on the board"
+  #if turn is valid
+  there_turns -= 1 if(true)
+end
+
+puts "winer is X"
