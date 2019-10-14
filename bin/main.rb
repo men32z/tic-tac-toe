f94ac63a97b713e31ce6893314ca9afd3f84a7f9
jstiven01 <stiven10@gmail.com>
2019-10-14 15:52:54 -0500

    Adding main.rb with rubocop suggestions

--- bin/main.rb
+++ bin/main.rb
@@ -1,47 +1,46 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require_relative '../lib/game'
 require_relative '../lib/user'
-puts "Hello welcome to the Tic Tac Toe Game"
+puts 'Hello welcome to the Tic Tac Toe Game'
+userx = User.new('X')
+userx.set_name
+usery = User.new('O')
+usery.set_name
+puts '--------------------------------------'
+puts 'This our board'
 
-puts "What is your name? (player X)"
-userx = User.new
-userx.name = gets.chomp
-puts "What is your name? (player O)"
-usery = User.new(gets.chomp)
-puts "--------------------------------------"
-puts "This our board"
-
-game = Game.new(userx,usery)
+game = Game.new(userx, usery)
 
 puts game.show_board
 flag = true
 while flag
   flag = false
-  while game.total_turns > 0
+  while game.total_turns.positive?
     puts "#{game.current_turn == 'X' ? userx.name : usery.name} pick a number to set the turn '#{game.current_turn}' "
     position = gets.chomp
-    
-    puts "--------------------------------------"
+    puts '--------------------------------------'
     status_game = game.move_game(position.to_i)
     if status_game
       puts status_game
     else
-      puts "Next turn"
+      puts 'Next turn'
     end
 
-    puts "--------------------------------------"
+    puts '--------------------------------------'
     puts game.show_board
   end
 
-  puts ""
-  puts "Do you want to play again? (y/n):"
+  puts ''
+  puts 'Do you want to play again? (y/n):'
   answer = gets.chomp
   if answer.downcase == 'y'
     flag = true
     game.reset_game
-    puts "--------------------------------------"
+    puts '--------------------------------------'
     puts game.show_board
   else
-    puts "Bye Bye!!"
+    puts 'Bye Bye!!'
   end
 end
