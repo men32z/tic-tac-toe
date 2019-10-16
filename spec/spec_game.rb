4e7121254216189d0f0c2d11e098b25a8730946d
Luis Preza <preza.luis@gmail.com>
2019-10-16 11:54:54 -0500

    test case for winner method

--- spec/spec_game.rb
+++ spec/spec_game.rb
@@ -26,4 +26,22 @@ RSpec.describe Game do
       expect(game.invalid_position(1)).to eq(false)
     end
   end
+  describe '#winner' do
+    it 'return false if there is no winner' do
+      expect(game.move_game(1)).to eq(false)
+    end
+
+    it 'return the winner if there is one' do
+      game.move_game(1)
+      game.move_game(4)
+      game.move_game(2)
+      game.move_game(5)
+      position = 3
+      game.instance_eval('@board[position - 1] = \'X\'') # rubocop:disable Style/EvalWithLocation
+      game.instance_eval('@turn_x = !@turn_x') # rubocop:disable Style/EvalWithLocation
+      game.instance_eval('@total_turns -= 1') # rubocop:disable Style/EvalWithLocation
+
+      expect(game.send(:winner, position - 1)).to eq('X')
+    end
+  end
 end
